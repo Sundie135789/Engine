71c70f48200a8bd3b66da2bd9629cac493fe8407
Sundie135789 <bhandarimudit5@gmail.com>
2026-03-12 19:09:26 +0530

    remove comment

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 /*
- The model matrix is handled by a Gameobject's Transform class.
- The projection and view matrices require a separate Camera class, even if there is only one Camera (uptill now).
- This is because the code inside a Camera class, written raw inside the main function in main.cpp would be catastrophic for cleanliness;
+ he model matrix is handled by a Gameobject's Transform class
+ The projection and view matrices require a separate Camera class, even if there
+ is only one Camera (uptill now). This is because the code inside a Camera
+ class, written raw inside the main function in main.cpp would be catastrophic
+ for cleanliness;
  * */
-
 
 /*
- The Font atlas is a PNG file containing all the characters of a font, tightly-packed together.
-  We load a font atlas from CPU side to the GPU as GL_TEXTURE_2D.
-  Whenever we need a character for rendering, we can look at the fnt data, look at the atlas, and get the character.
-  This is how bitmap font texture atlas system works.
+ The Font atlas is a PNG file containing all the characters of a font,
+ tightly-packed together. We load a font atlas from CPU side to the GPU as
+ GL_TEXTURE_2D. Whenever we need a character for rendering, we can look at the
+ fnt data, look at the atlas, and get the character. This is how bitmap font
+ texture atlas system works.
  * */
-// remove sidepane and implement bottom pane, toggled GUI, upon change/non changeable key like `
 /*
  *+------------------------------------------------+
 |                                                |
@@ -23,141 +24,130 @@
 | > _                                            |     above input line
 +------------------------------------------------+
  * */
-#include <iostream>
-#include <GL/glew.h>
-#include <GL/gl.h>
-#include <GLFW/glfw3.h>
-#include "headers/mesh.hpp"
-#include "headers/shader.hpp"
+// TODO: refactor side pane -> implement ImGui.
+// TODO: https://character.ai/chat/SeebJuz_llkYvFhSUGdWhuUEh2TtciQIBIdbLpkWDs4
+//  TODO: start from nvim-treesitter.
+//  https://claude.ai/chat/0a3c8bd0-fd49-4a5c-b0e9-6f6610c10009
+// TODO: -->
+// !-->
+#include "assets/imgui.h"
+#include "assets/imgui_impl_glfw.h"
+#include "assets/imgui_impl_opengl3.h"
+#include "assets/imgui_stdlib.h"
+#include "headers/camera.hpp"
 #include "headers/gameobject.hpp"
 #include "headers/global.hpp"
-#include "headers/texture.hpp"
-#include "headers/sidepane.hpp"
-#include "headers/transform.hpp"
-#include "headers/camera.hpp"
-#include "headers/text.hpp"
 #include "headers/lang.hpp"
-#include <fstream>
+#include "headers/mesh.hpp"
+#include "headers/shader.hpp"
+#include "headers/texture.hpp"
+#include "headers/transform.hpp"
 #include "stb_image.h"
+#include <GL/gl.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include <iostream>
 #define WINDOW_WIDTH 2560
 #define WINDOW_HEIGHT 1920
-std::vector<GameObject*> gameobjects;
+std::vector<GameObject *> gameobjects;
 std::string commandBuffer = "";
-bool LoadFont(const std::string& fntFile, const std::string& atlasFile){
-  std::ifstream file(fntFile);
-  if(!file.is_open()) return false;
-  std::string line;
-  while(std::getline(file, line)){
-    if(!line.empty() && line.back() == '\r')line.pop_back();
-    if(line.find("common ") == 0){
-      sscanf(line.c_str(), "common lineHeight=%f base=%*f ascent=%*f descent=%*f scaleW=%d scaleH=%d",
-          &fontLineHeight, &fontAtlasWidth, &fontAtlasHeight);
-  
-    }
-    else if(line.find("char id=") == 0){
-      int id, x, y, width, height;
-      float xOffset, yOffset, xAdvance;
-      sscanf(line.c_str(), "char id=%d x=%d y=%d width=%d height=%d xoffset=%f yoffset=%f xadvance=%f",
-          &id, &x, &y, &width, &height, &xOffset, &yOffset, &xAdvance);
-      Character ch((char) id, x, y, width, height, xOffset, yOffset, xAdvance);
-      fontCharacters[(char)id] = ch;
-    }
-  }
-  file.close();
-  for(auto& [key,ch] : fontCharacters){
-    ch.u0 /=fontAtlasWidth;
-    ch.u1 /= fontAtlasWidth;
-    ch.v0 /= fontAtlasHeight;
-    ch.v1 /= fontAtlasHeight;
-  }
-  int width, height, channels;
-  unsigned short* data = stbi_load_16(atlasFile.c_str(), &width, &height, &channels, 0);
-  if(!data) return false;
-  glGenTextures(1, &fontTextureID);
-  glBindTexture(GL_TEXTURE_2D, fontTextureID);
-  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-  glTexImage2D (GL_TEXTURE_2D, 0, GL_R16, width, height, 0,GL_RED, GL_UNSIGNED_SHORT, data);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  stbi_image_free(data);
-  return true;
-
-}
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods){
-  if(action != GLFW_PRESS) return;
-  if(key >= GLFW_KEY_A && key <= GLFW_KEY_Z){
+bool commandEnabled = false;
+void key_callback(GLFWwindow *window, int key, int scancode, int action,
+                  int mods) {
+  /*std::cout << "VALUE OF COMMAND ENABLED: " << std::to_string(commandEnabled)
+   * << std::endl;*/
+  ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
+  if (action != GLFW_PRESS)
+    return;
+  if (key >= GLFW_KEY_A && key <= GLFW_KEY_Z) {
     char c = 'a' + (key - GLFW_KEY_A);
-    commandBuffer += c;
-  }else if(key == GLFW_KEY_SPACE){
-    commandBuffer += " ";
-  }
-  else if(key == GLFW_KEY_ENTER){
-    run_command(commandBuffer);
-    commandBuffer = "";
-  }else if(key == GLFW_KEY_BACKSPACE){ 
-    if(commandBuffer != ""){
+    if (commandEnabled)
+      commandBuffer += c;
+  } else if (key == GLFW_KEY_SPACE) {
+    if (commandEnabled)
+      commandBuffer += " ";
+  } else if (key == GLFW_KEY_ENTER) {
+    if (commandEnabled) {
+      run_command(commandBuffer);
+      commandBuffer = "";
+    }
+  } else if (key == GLFW_KEY_BACKSPACE) {
+    if (commandEnabled && commandBuffer != "") {
       commandBuffer.pop_back();
     }
+  } else if (key == GLFW_KEY_MINUS) {
+    commandEnabled = !commandEnabled;
   }
 }
-void goTerminate(){
-  for(auto g : gameobjects){
+void goTerminate() {
+  for (auto g : gameobjects) {
     delete g;
   }
   gameobjects.clear();
   glfwTerminate();
-
 }
-int main(void){
-  if(!glfwInit()){
+int main(void) {
+  if (!glfwInit()) {
     std::cout << "GLFW Init error" << std::endl;
     goTerminate();
     return 1;
   }
-  GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "GameEngine - SundieCoder v1.0", nullptr, nullptr);
+  GLFWwindow *window =
+      glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+                       "GameEngine - SundieCoder v1.0", nullptr, nullptr);
   glfwMakeContextCurrent(window);
   glewExperimental = GL_TRUE;
   GLenum glewStatus = glewInit();
-  if(glewStatus != GLEW_OK){
+  if (glewStatus != GLEW_OK) {
     std::cout << "GLEW Init error" << std::endl;
     goTerminate();
     return 1;
   }
   glfwSetKeyCallback(window, key_callback);
-  glViewport(0,0,WINDOW_WIDTH, WINDOW_HEIGHT);
+  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
   // --- OpenGL Code ---
-  float command_default_color[] = {0.403f, 0.403f, 0.403f};
-  float command_color[] = {0.675, 0.086, 0.173};
-  
-  Camera* mainCamera = new Camera((float)WINDOW_WIDTH/WINDOW_HEIGHT);
-  loadSidePane();
-  loadCommandField();
-  LoadFont("assets/description.fnt", "assets/font.png");
-  Shader* textShader = new Shader("shaders/text.vert", "shaders/text.frag");
-  glm::mat4 textOrtho = glm::ortho(0.0f, (float) WINDOW_WIDTH, (float)WINDOW_HEIGHT, 0.0f,-1.0f, 1.0f);
-  textShader->use();
-  textShader->SetMatrix("projection", textOrtho);
-  initTextRender();
-  float vertices[] = {
-    -0.375f, -0.375f, 0.0f, 0.0f, 0.0f,
-    0.375f, -0.375f, 0.0f, 1.0f, 0.0f,
-    0.0f, 0.375f, 0.0f ,0.5f, 1.0f
-  };
-  Texture* texture = new Texture("assets/monkey.png");
-  Mesh* mesh = new Mesh(vertices, 3, true);
-  Shader* shader = new Shader("shaders/texture.vert", "shaders/texture.frag");
-  Transform* transform = new Transform();
-  //shader->SetColor(0.5f, 0.5f, 0.5f);
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  io.IniFilename = nullptr;
+  io.ConfigFlags |=
+      ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+  // Setup Platform/Renderer backends
+  ImGui_ImplGlfw_InitForOpenGL(
+      window, false); // Second param install_callback=true will install GLFW
+                      // callbacks and chain to existing ones.
+  ImGui_ImplOpenGL3_Init();
+  ImGui::StyleColorsDark();
+
+  // Loading Font
+  io.FontDefault =
+      io.Fonts->AddFontFromFileTTF("assets/IosevkaCustom-Regular.ttf", 22.0f);
+  ImGuiStyle &style = ImGui::GetStyle();
+  style.FrameRounding = 4.0f;
+  style.WindowRounding = 6.0f;
+  Camera *mainCamera = new Camera((float)WINDOW_WIDTH / WINDOW_HEIGHT);
+  float vertices[] = {-0.375f, -0.375f, 0.0f, 0.0f, 0.0f,
+                      0.375f,  -0.375f, 0.0f, 1.0f, 0.0f,
+                      0.0f,    0.375f,  0.0f, 0.5f, 1.0f};
+  Texture *texture = new Texture("assets/monkey.png");
+  Mesh *mesh = new Mesh(vertices, 3, true);
+  Shader *shader = new Shader("shaders/texture.vert", "shaders/texture.frag");
+  Transform *transform = new Transform();
   shader->SetTexture(texture);
-  //shader->SetTexture(text);
-  GameObject* gameobject = new GameObject("triangle");
+  GameObject *gameobject = new GameObject("triangle");
   gameobject->SetShader(shader);
   gameobject->SetMesh(mesh);
   gameobject->SetTransform(transform);
   gameobjects.push_back(gameobject);
-  
-  while(!glfwWindowShouldClose(window)){
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
@@ -165,27 +155,46 @@ int main(void){
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     assert(mainCamera != nullptr);
-    
-    for(GameObject* g : gameobjects){
+
+    for (GameObject *g : gameobjects) {
       g->draw(*mainCamera);
     }
-    glUseProgram(sidePaneShaderProgram);
-    glBindVertexArray(sidePaneVAO); 
-    glUniform3f(glGetUniformLocation(sidePaneShaderProgram, "uColor"), 0.133f, 0.133f, 0.133f);
-    glDisable(GL_DEPTH_TEST);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
-    glDisable(GL_DEPTH_TEST);
-    if (commandBuffer == "") renderText(*textShader, "Enter Command Here...", 2112.5f, WINDOW_HEIGHT - 75.0f, 1.0f, command_default_color);
-    else renderText(*textShader, commandBuffer, 2112.5f, WINDOW_HEIGHT-75.0f, 1.0f, command_color);
-    glLineWidth(3.0f);
-    glBindVertexArray(commandFieldVAO);
-    glUseProgram(commandFieldShader->shaderProgram);
-    glDrawArrays(GL_LINE_LOOP, 0, 4);
-    glEnable(GL_DEPTH_TEST);
+
+    if (commandEnabled) {
+      ImGui::SetNextWindowPos(ImVec2(0, 1020), ImGuiCond_FirstUseEver);
+      ImGui::SetNextWindowSize(ImVec2(WINDOW_WIDTH, 300),
+                               ImGuiCond_FirstUseEver);
+      ImGui::Begin("Terminal");
+      ImGui::InputText("Command : ", &commandBuffer);
+      ImGui::End();
+      /* glUseProgram(sidePaneShaderProgram);
+       glBindVertexArray(sidePaneVAO);
+       glUniform3f(glGetUniformLocation(sidePaneShaderProgram, "uColor"),
+       0.133f, 0.133f, 0.133f); glDisable(GL_DEPTH_TEST);
+       glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+       glBindVertexArray(0);
+       glDisable(GL_DEPTH_TEST);
+       if (commandBuffer == "") renderText(*textShader, "Enter Command Here...",
+       1012.5f, WINDOW_HEIGHT - 75.0f, 1.0f, command_default_color); else
+       renderText(*textShader, commandBuffer, 1012.5f,
+       WINDOW_HEIGHT-75.0f, 1.0f, command_color); glLineWidth(3.0f);
+       glBindVertexArray(commandFieldVAO);
+       glUseProgram(commandFieldShader->shaderProgram);
+       glDrawArrays(GL_LINE_LOOP, 0, 4);
+       glEnable(GL_DEPTH_TEST);
+       //separator line
+       separatorader->use();
+       glBindVertexArray(separatorVAO);
+       glDrawArrays(GL_LINES, 0, 2);
+       glBindVertexArray(0);*/
+    }
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     glfwSwapBuffers(window);
-    glfwPollEvents();
   }
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
   glfwDestroyWindow(window);
   goTerminate();
   return 0;
