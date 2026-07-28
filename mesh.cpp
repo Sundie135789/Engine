dbb404afbaea55727e2f0c416735577d5bc6849f
Mudit <bhandarimudit5@gmail.com>
2026-07-28 16:38:34 +0530

    fixed drawElements issue

--- mesh.cpp
+++ mesh.cpp
@@ -9,7 +9,7 @@ Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices){
   glBindVertexArray(vao);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices.data(), GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
   glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(),vertices.data(), GL_STATIC_DRAW);
   glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
@@ -24,5 +24,5 @@ Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices){
 void Mesh::Draw() const{
   glBindVertexArray(vao);
   //glDrawArrays(GL_TRIANGLES, 0, vertices.size());
-  glDrawElements(GL_TRIANGLES, 0, GL_UNSIGNED_INT, 0);
+  glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
 }
